1d2eff99101165b7847c74cd4f5d57a468e585e3
Obua Emmanuel <obuaemmanuel10@gmail.com>
2023-05-15 13:30:26 +0300

    sample test application ...

--- src/test/java/com/yukon/app/sch/SchApplicationTests.java
+++ src/test/java/com/yukon/app/sch/SchApplicationTests.java
@@ -1,13 +1,36 @@
 package com.yukon.app.sch;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SchApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.yukon.app.sch.repository.StudentRepository;
+import com.yukon.app.sch.model.Student;
+
+@DataJpaTest
+public class SchApplicationTests {
+
+	@Autowired
+	private StudentRepository studentRepository;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testFindAll() {
+    // given
+		Student student1 = new Student("John", "Doe");
+		Student student2 = new Student("Jane", "Doe");
+		studentRepository.save(student1);
+		studentRepository.save(student2);
 
+    // when
+		List<Student> students = studentRepository.findAll();
+
+    // then
+		assertThat(students).hasSize(2);
+		assertThat(students).contains(student1, student2);
+	}
 }
