e2a0dce19402e3e4e81313254227a8ab2527c544
Obua Emmanuel <obuaemmanuel10@gmail.com>
2023-05-15 16:35:23 +0300

    Type hinting the Repositories

--- src/main/java/com/yukon/app/sch/repository/StudentRepository.java
+++ src/main/java/com/yukon/app/sch/repository/StudentRepository.java
@@ -1,8 +1,10 @@
 package com.yukon.app.sch.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.yukon.app.sch.model.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
--- src/main/java/com/yukon/app/sch/repository/TeacherRepository.java
+++ src/main/java/com/yukon/app/sch/repository/TeacherRepository.java
@@ -1,8 +1,10 @@
 package com.yukon.app.sch.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.yukon.app.sch.model.Teacher;
 
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
 }
