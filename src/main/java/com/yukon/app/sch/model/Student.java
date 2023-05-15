23929271ade7c44df2d736d763139e327448a523
Obua Emmanuel <obuaemmanuel10@gmail.com>
2023-05-15 16:37:17 +0300

    minor adjustments in the model settings

--- src/main/java/com/yukon/app/sch/model/Student.java
+++ src/main/java/com/yukon/app/sch/model/Student.java
@@ -1,21 +1,22 @@
 package com.yukon.app.sch.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "students")
 public class Student {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     private String surname;
 
     public Student(String name, String surname) {
--- src/main/java/com/yukon/app/sch/model/Teacher.java
+++ src/main/java/com/yukon/app/sch/model/Teacher.java
@@ -1,13 +1,16 @@
 package com.yukon.app.sch.model;
 
 import java.util.List;
-// import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "teachers")
 public class Teacher {
