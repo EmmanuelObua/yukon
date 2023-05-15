aa0b6be46dc2c1578b6cc219d0e6dd8de1766c2c
Obua Emmanuel <obuaemmanuel10@gmail.com>
2023-05-15 13:18:58 +0300

    fixd errors with th SchAppliation main class

--- src/main/java/com/yukon/app/sch/SchApplication.java
+++ src/main/java/com/yukon/app/sch/SchApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-// @ComponentScan(basePackages = {"com.yukon.app.sch"})
 public class SchApplication {
 
 	public static void main(String[] args) {
